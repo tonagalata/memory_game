331d61d0e6b9583f16f82790eb0bc99f98f61540
Galata Tona <galata@Galatas-MBP.fios-router.home>
2019-10-20 00:43:01 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,35 @@
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+// let cardOne = "queen";
+// let cardTwo = "queen";
+// let cardThree = "king";
+// let cardFour = "king";
 
 
+// console.log("User flipped " + cardThree);
+
+const cards = ["queen", "queen", "king", "king"];
+
+const cardsInPlay = [];
+
+let cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+let cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+// let cardThree = cards[2];
+
+// cardsInPlay.push(cardThree);
+
+// let cardFour = cards[3];
+
+// cardsInPlay.push(cardFour);
+
+
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
