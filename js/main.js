b639e710f07d53cebf008b2f14b566e3e304d231
Galata Tona <galata@Galatas-MBP.fios-router.home>
2019-10-21 19:51:39 -0400

    added more logic to game

--- js/main.js
+++ js/main.js
@@ -10,26 +10,25 @@ const cards = ["queen", "queen", "king", "king"];
 
 const cardsInPlay = [];
 
-let cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-let cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-// let cardThree = cards[2];
-
-// cardsInPlay.push(cardThree);
-
-// let cardFour = cards[3];
-
-// cardsInPlay.push(cardFour);
-
-
-
-if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again.");
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} 	else {
+		alert("Sorry, try again.");
+	}
 }
+
+function flipCard(cardID) {
+
+	console.log("User flipped " + cards[cardID]);
+	
+	checkForMatch();
+
+	cardsInPlay.push(cards[cardID]);
+
+}
+
+flipCard(0);
+flipCard(2);
+
+
